299a2efda2886423908ebaccf2105a25436ef808
Varad <vaidyavarad2001@gmail.com>
2021-06-30 23:48:40 +0530

    cleaned up the code and fixed the weight solving issue

--- dynamical_systems/functionApprox.py
+++ dynamical_systems/functionApprox.py
@@ -46,7 +46,8 @@ class functionApprox():
         
         # ## calculate the centers
         
-        center = np.exp(-firstorderSystem.alpha * np.linspace(0,1,numBasis) * firstorderSystem.totaltime)
+        #center = np.exp(-firstorderSystem.alpha * np.linspace(0,1,numBasis) * firstorderSystem.totaltime)
+        center = np.exp(-firstorderSystem.alpha * des_activation)
         width = 1/np.gradient(center)**2
         
         # for i in range(numBasis):
@@ -90,6 +91,7 @@ class functionApprox():
         PSI,values,weight_values = numBasis*[None], numBasis*[None], numBasis*[None]
         
         centers,widths,weights =  np.linspace(0,firstorderSystem.totaltime,numBasis),np.random.randint(1,3,size=(numBasis)) / 4 ,np.random.rand(numBasis)
+        weights = np.array(weights)
         
         for i in range(numBasis):
             PSI[i] = Gaussian(widths[i], centers[i],weights[i])
@@ -100,7 +102,7 @@ class functionApprox():
         weight_values = np.array(weight_values).T
         print("shape weight_values:", np.shape(weight_values))
         weight_gaussian = np.sum(weight_values,axis=1)
-        
+        print("forcing func", weight_gaussian)
         return weight_gaussian
         
     def get_weights(forcing_function,PSI,gaussian_values,firstorderSystem,numBasis):
@@ -142,15 +144,15 @@ class functionApprox():
         # for i in range(numBasis):
         #     PSI_matrix[:,i] = PSI[i].evaluate(xvalues) *xvalues / np.sum(gaussian_values,axis=1)
         
-        # for xval in range((len(xvalues))):
+        for xval in range((len(xvalues))):
             
-        #     denomSum = 0
+            denomSum = 0
             
-        #     for N in range(numBasis):
-        #         PSI_matrix[xval,N] = PSI[N].evaluate(xvalues[xval]) * xvalues[xval]
-        #         denomSum = denomSum + PSI[N].evaluate(xvalues[xval])
+            for N in range(numBasis):
+                PSI_matrix[xval,N] = PSI[N].evaluate(xvalues[xval]) * xvalues[xval]
+                denomSum = denomSum + PSI[N].evaluate(xvalues[xval])
             
-        #     PSI_matrix[xval] = PSI_matrix[xval]/ denomSum
+            PSI_matrix[xval] = PSI_matrix[xval]/ denomSum
         
             
         weights = np.linalg.lstsq(PSI_matrix,forcing_function,rcond=None)[0]
@@ -191,30 +193,22 @@ if __name__ == '__main__':
     from dynamical_systems.canonical import FirstOrderDynamicalSystems
     from dynamical_systems.canonical import SecondOrderDynamicalSystem
     
-    totaltime = 5
+    totaltime = 10
     phaseSystem = FirstOrderDynamicalSystems(alpha = 1,totaltime= totaltime)
     dynamicalSystem = SecondOrderDynamicalSystem(alpha =1,goal = 2,totaltime= totaltime)
     xvalues = phaseSystem.simulation()
-    print("xvalues \n",xvalues)
-    numBasis = 10
+    numBasis = 100
     forcingfunc = functionApprox.generateRandomForcingFunc(numBasis,phaseSystem)
-    # forcingfunc[0:1000] ,forcingfunc[-1000:-1] = 0,0
-    # forcingfunc[-1] = 0
-    if len(phaseSystem.simulation()) != len(forcingfunc):
-        print("fucked")
     
-    PSI,gaussian_values = functionApprox.create_Gaussian_basis(numBasis,phaseSystem,ifPlot = True)
+    
+    PSI,gaussian_values = functionApprox.create_Gaussian_basis(numBasis,phaseSystem,ifPlot = False)
     
     
     if len(forcingfunc) != len(phaseSystem.timearray):
         raise ValueError("forcing func size invalid")
-    print("stagw 1")
     #weights = functionApprox.get_weights(forcingfunc,PSI,gaussian_values,phaseSystem,numBasis)
-    #weights = functionApprox.get_weightsLWR(PSI,forcingfunc,numBasis,phaseSystem,dynamicalSystem)\
+    #weights = functionApprox.get_weightsLWR(PSI,forcingfunc,numBasis,phaseSystem,dynamicalSystem)
     weights = functionApprox.get_weights_lstsquare(forcingfunc,PSI,gaussian_values,phaseSystem,numBasis)
-    print("weights shape",weights.shape)
-    print("weights",weights)
-    print("stage 2")
     weighted_evaluate = [None] * numBasis
     for i in range(numBasis):
         PSI[i].weight = weights[i]
@@ -222,8 +216,8 @@ if __name__ == '__main__':
     
     print("satgw 2")
     weighted_evaluate = np.array(weighted_evaluate).T
-    approximatedFunc = np.sum(weighted_evaluate,axis=1)
-    
+    approximatedFunc = np.sum(weighted_evaluate,axis=1) * xvalues/np.sum(gaussian_values,axis=1)
+    # print("approximated func",approximatedFunc)
     import matplotlib.pyplot as plt
     plt.plot(phaseSystem.timearray,forcingfunc,label = "forcing func")
     plt.plot(phaseSystem.timearray,approximatedFunc,label="approximatedFunc")
