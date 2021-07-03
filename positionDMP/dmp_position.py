cf790280d3bd516f0b54842e8f46116911c8b9a4
Varad <vaidyavarad2001@gmail.com>
2021-07-03 15:38:17 +0530

    added cartesian DMP in 3D

--- positionDMP/dmp_position.py
+++ positionDMP/dmp_position.py
@@ -19,19 +19,18 @@ class Gaussian():
     
 class PositionDMP():
     
-    def __init__(self,alpha=1,N_bfs = 10,cs_alpha = None,totaltime = 10,cs_tau = 1):
+    def __init__(self,alpha,cs_alpha,N_bfs = 10,totaltime = 10,cs_tau = 1):
         
         self.N_bfs = N_bfs
         self.alpha = alpha
         self.beta = self.alpha/4
         self.totaltime = totaltime
-        if cs_alpha is None:
-            self.cs_alpha = self.alpha/2
-        else:
-            self.cs_alpha = cs_alpha
-        self.t = np.linspace(0,totaltime,int(totaltime/0.01))
-        self.dt = np.gradient(self.t)
+        self.cs_alpha = cs_alpha
+        self.t = np.linspace(0,totaltime,int(totaltime/0.01) + 1)
+        self.dt = self.t[1] - self.t[0]
         
+        # scaling factor.
+        self.Dp = np.identity(3)
         
         self.cs = CanonicalSystem(alpha = self.cs_alpha,t=self.t,tau= cs_tau)
         
@@ -42,11 +41,19 @@ class PositionDMP():
         # variance of the basis functions
         self.h = 1.0/np.gradient(self.c)**2
         
-        self.w = np.zeros((N_bfs))
+        self.w = np.zeros((3,N_bfs))
         
-        self.initPos = 0
-        self.goalPos = 0
+        self.initPos = np.zeros(3)
+        self.goalPos = np.zeros(3)
+        
+        self.reset()
     
+    def reset(self):
+        self.p = self.initPos.copy()
+        self.dp = np.zeros(3)
+        self.ddp = np.zeros(3)
+        self.cs.reset()
+        
     ## placeholder function to generate Random Forcing Func
     def generateRandomForcingFunc(self):
         '''
@@ -70,19 +77,33 @@ class PositionDMP():
         #print("forcing func", weight_gaussian)
         return weight_gaussian
     
-    
     def approximatedFunc(self,x,w):
         
-        weight_sum,psi_sum = np.zeros_like(x),np.zeros_like(x)
-        for i in range(self.N_bfs):
+        # weight_sum,psi_sum = np.zeros_like(x),np.zeros_like(x)
+        # for i in range(self.N_bfs):
             
-            psi_i = np.exp(-self.h[i] * (x -self.c[i]) **2)
-            w_psi = w[i] * psi_i
+        #     psi_i = np.exp(-self.h[i] * (x -self.c[i]) **2)
+        #     w_psi = w[i] * psi_i
             
-            weight_sum += w_psi
-            psi_sum += psi_i
+        #     weight_sum += w_psi
+        #     psi_sum += psi_i
         
-        approximatedFunc = weight_sum * x / psi_sum
+        # approximatedFunc = weight_sum * x / psi_sum
+        # return approximatedFunc
+        
+        def forcing_(xi):
+            
+            psi = np.exp(-self.h* (xi - self.c) **2)
+            
+            f_ = (xi * self.w.dot(psi)/psi.sum())
+
+            return  f_
+        
+        approximatedFunc = np.empty_like(x)
+        
+        for i in range(len(x)):
+            approximatedFunc[i] = forcing_(x[i])
+        
         return approximatedFunc
     
     def train(self,position):
@@ -98,24 +119,67 @@ class PositionDMP():
         self.initPos = position[0]
         self.goalPos = position[-1]
         
-        x = self.cs.rollout()
+        print("goalPos",self.goalPos)
+        print("initPos",self.initPos)
+        self.x = self.cs.rollout()
                            
+        # scaling factor         
+        self.Dp = np.diag(self.goalPos - self.initPos)
+        Dp_inv = np.linalg.inv(self.Dp)
         
-        des_dp = np.gradient(position)/self.dt
-        des_ddp = np.gradient(des_dp)/self.dt
+        des_dp = np.gradient(position,axis=0)/self.dt
+        des_ddp = np.gradient(des_dp,axis=0)/self.dt
         
         def forcing(i):
-            f_i = self.cs.tau**2 * des_ddp[i] - self.alpha * ( (self.beta * (self.goalPos - position[i]) ) - self.cs.tau*des_dp[i] )
-            return f_i
+            f_i = self.cs.tau**2 * des_ddp[i] - self.alpha * ( self.beta * (self.goalPos - position[i])  - self.cs.tau*des_dp[i] )
+            scaled_f_i =  Dp_inv.dot(f_i) 
+            return scaled_f_i
         
-        A = np.stack(feature_(xj) for xj in x)
+        A = np.stack(feature_(xj) for xj in self.x)
         #forcingfunc = self.generateRandomForcingFunc()
         forcingfunc = np.stack(forcing(i) for i in range(len(self.t)))
-        self.w = np.linalg.lstsq(A,forcingfunc,rcond=None)[0]
-        #print(self.w)
-        approximatedFunc = self.approximatedFunc(x,self.w)
-        self.plotStuff(forcingfunc,approximatedFunc)
-        self.plotGaussians(x)
+        
+        self.w = np.linalg.lstsq(A,forcingfunc,rcond=None)[0].T
+        
+        # approximatedFunc = self.approximatedFunc(self.x,self.w)
+        # self.plotStuff(forcingfunc,approximatedFunc)
+        # self.plotGaussians(self.x)
+        #plt.plot(self.t,(forcingfunc-approximatedFunc))
+        self.trained_p = position
+        self.trained_des_dp = des_dp
+        self.trained_des_ddp = des_ddp
+    
+    def step(self,x):
+        def forcing_(xi):
+            
+            psi = np.exp(-self.h* (xi - self.c) **2)
+            
+            f_ = self.Dp.dot(xi * self.w.dot(psi)/psi.sum() )
+            return  f_
+
+        
+        self.ddp = (self.alpha * (self.beta * (self.goalPos - self.p) - self.cs.tau* self.dp ) + forcing_(x) ) * 1/self.cs.tau
+        
+        self.dp += self.ddp * self.dt
+        
+        self.p += self.dp * self.dt
+        return self.p, self.dp, self.ddp ## ~ returned to plot stuff later on
+        
+    def rollout(self):
+        
+        self.reset()
+        
+        #x = self.cs.rollout()
+        
+        dmp_position = np.empty_like(position)
+        
+        # ^ note to self. for future reference dt can be passed element by element if dt has to be a numpy vector 
+        for i in range(len(self.t)):
+            dmp_position[i],_,__ = self.step(self.x[i])
+        return dmp_position
+        
+        
+            
     def plotGaussians(self,x):
         
         gaussianMatrix = np.empty(( self.N_bfs, len(self.t) ))
@@ -136,9 +200,28 @@ class PositionDMP():
 if __name__ =="__main__":
     
     import matplotlib.pyplot as plt
-    dmp = PositionDMP(N_bfs=30)
-    position = np.polyval([1,6,11,6],dmp.t)
+    
+    dmp = PositionDMP(N_bfs=200,alpha= 10,cs_alpha=1,totaltime = 5)
+    #position = np.array([np.polyval([1,-2,3,4],dmp.t),np.polyval([-1,2,-3,4],dmp.t),np.polyval([-1,2,-3,4],dmp.t)]).T
+    position = np.array([np.sin(dmp.t),np.sin(dmp.t),np.sin(dmp.t)]).T
+
+    # np.savetxt("position.csv",position,fmt="%f",delimiter=",")
     dmp.train(position)
+    # np.savetxt("dmp accel.csv",dmp.trained_des_ddp,fmt="%f",delimiter=",")
+    # np.savetxt("dmp velocity.csv",dmp.trained_des_dp,fmt="%f",delimiter=",")
+    dmp_position = dmp.rollout()
+    # np.savetxt("dmp_position.csv",dmp_position,fmt="%f",delimiter=",")
+    ## plotting the euclidiean difference between dmp position and the given position
+    
+    euclidiean_diff = position - dmp_position
+    plt.figure()
+    plt.plot(dmp.t,euclidiean_diff[:,0],label="error between the trajectories")
+    plt.plot(dmp.t,position[:,0],label="given position")
+    plt.plot(dmp.t,dmp_position[:,0],label="dmp position")
+    plt.legend()
+    plt.show()
+    
+    
     
     
     
