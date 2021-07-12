d499d23acbebfa087b139233989f9d6f712050a0
Varad <vaidyavarad2001@gmail.com>
2021-07-13 00:00:55 +0530

    added 2D and 3D obstacle avoidance

--- examples/avoid_obstacle.py
+++ examples/avoid_obstacle.py
@@ -6,6 +6,64 @@ from positionDMP.dmp_position import PositionDMP
 from obstacles.obstacle import Obstacle
 from utils.trajFuncs import generate3DTraj
 
+## init the obstacle
+o1 = Obstacle(initPos=np.array([0,0,-2]))
+dmp = PositionDMP(N_bfs=100,alpha= 30,cs_alpha=5,totaltime = 5,obstacle = o1) ## ^ init the DMP class.
 
+initPos,initVel,finalPos = np.array([
+    [-2,-2,-2],
+    [0,0,0],
+    [1,1,-2.001],
+])
+
+position = generate3DTraj(initPos,initVel,finalPos,dmp.totaltime,dmp.t)
+dmp.train(position) ## ^ train the DMP
+
+dmp_position = dmp.rollout(position) ## ^ simulate
+
+euclidiean_norm = np.linalg.norm((position - dmp_position) , axis= 1) ## ^ euclidiean norm
+
+## plot stuff...
+
+## ? 2D plots.
+fig,ax = plt.subplots(4,1,sharex= True)
+
+ax[0].plot(dmp.t,position[:,0],label='Demo')
+ax[0].plot(dmp.t,dmp_position[:,0],label='DMP')
+ax[0].set_xlabel('t')
+ax[0].set_ylabel('X')
+
+ax[1].plot(dmp.t,position[:,1],label='Demo')
+ax[1].plot(dmp.t,dmp_position[:,1],label='DMP')
+ax[1].set_xlabel('t')
+ax[1].set_ylabel('Y')
+
+ax[2].plot(dmp.t,position[:,2],label='Demo')
+ax[2].plot(dmp.t,dmp_position[:,2],label='DMP')
+ax[2].set_xlabel('t')
+ax[2].set_ylabel('z')
+ax[2].legend()
+
+ax[3].plot(dmp.t,euclidiean_norm,label='Error Norm')
+ax[3].legend()
+
+
+
+
+
+## ^ uncommented the below code as the below is for 3D obstacle.
+# ## ? 3D plot.
+
+# fig3D = plt.figure()
+# ax3D = plt.axes(projection = '3d')
+# ax3D.plot3D(position[:,0],position[:,1],position[:,2],label='Demo')
+# ax3D.plot3D(dmp_position[:,0],dmp_position[:,1],dmp_position[:,2],label='DMP')
+# ax3D.plot([o1.initPos[0]],[o1.initPos[1]],[o1.initPos[2]], markerfacecolor='k', markeredgecolor='k', marker='o', markersize=5)
+# ax3D.set_xlabel('X')
+# ax3D.set_ylabel('Y')
+# ax3D.set_zlabel('Z')
+# ax3D.legend()
+
+plt.show()
 
     
---
+++ examples/avoid_obstacle2D.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+sys.path.append( sys.path[0] +'/..')
+from positionDMP.dmp_position import PositionDMP
+from obstacles.obstacle import Obstacle
+from utils.trajFuncs import generate2DTraj
+
+o1 = Obstacle(initPos=np.array([0,0]),n_dim=2)
+
+dmp = PositionDMP(N_bfs=100,alpha= 30,cs_alpha=3,totaltime = 5,cs_tau = 1,n_dim = 2,obstacle = o1) ## ^ init the DMP class.
+
+initPos,initVel,finalPos = np.array([
+    [0,-1],
+    [0,1],
+    [2,3],
+])
+
+position = generate2DTraj(initPos,initVel,finalPos,dmp.totaltime,dmp.t)
+
+dmp.train(position) ## ^ train the DMP
+
+dmp_position = dmp.rollout(position) ## ^ simulate
+
+euclidiean_norm = np.linalg.norm((position - dmp_position) , axis= 1) ## ^ euclidiean norm
+
+## ? 2D plots
+
+fig,ax = plt.subplots(3,1,sharex= True)
+ax[0].plot(dmp.t,position[:,0],label='Demo')
+ax[0].plot(dmp.t,dmp_position[:,0],label='DMP')
+ax[0].set_xlabel('t')
+ax[0].set_ylabel('X')
+
+ax[1].plot(dmp.t,position[:,1],label='Demo')
+ax[1].plot(dmp.t,dmp_position[:,1],label='DMP')
+ax[1].set_xlabel('t')
+ax[1].set_ylabel('Y')
+ax[1].legend()
+
+ax[2].plot(dmp.t,euclidiean_norm,label='Error Norm')
+ax[2].legend()
+
+fig2,ax2 = plt.subplots(1,1)
+ax2.plot(position[:,0],position[:,1],label='demo')
+ax2.plot(dmp_position[:,0],dmp_position[:,1],label='dmp')
+ax2.plot(o1.initPos[0],o1.initPos[1],'ro')
+plt.show()
