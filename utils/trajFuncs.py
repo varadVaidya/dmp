9347a22646a602f7ac98703c14ff698fbaabc261
Varad <vaidyavarad2001@gmail.com>
2021-07-13 15:53:33 +0530

    added quaternion traj utils

--- utils/trajFuncs.py
+++ utils/trajFuncs.py
@@ -1,4 +1,5 @@
 import numpy as np
+import quaternionic as qt
 
 def generate3DTraj(initPos,initVel,finalPos,T,t):
     ## generates minimum jerk trajectory
@@ -51,9 +52,27 @@ def generate2DTraj(initPos,initVel,finalPos,T,t):
     posTraj = np.vstack((xTraj,yTraj)).T
     return posTraj
 
+
+def generateQuaternionTraj(initQuat,finalQuat,T):
+    
+    rotation = qt.interpolation.slerp(initQuat,finalQuat,T)
+    return rotation
+    
+def convertTo_pybulletQuat(quaternion):
+    
+    ## quaternion is of the class quaternionic
+    
+    pbQuaternion = np.zeros_like(quaternion.ndarray)
+    pbQuaternion[:,0:3] = quaternion.vector
+    pbQuaternion[:,3] = quaternion.real
+    
+    return pbQuaternion
+    
+    
+
 if __name__ == '__main__':
     
-    what_to_test = 1
+    what_to_test = 2
     
     if what_to_test == 0:
         
@@ -81,3 +100,12 @@ if __name__ == '__main__':
         import matplotlib.pyplot as plt
         plt.plot(t,x)
         plt.show()
+    
+    if what_to_test == 2:
+        q1 = qt.array([1,0,0,0])
+        q2 = qt.array([1,0.5,0.5,0.75])
+        
+        rotation = qt.interpolation.slerp(q1, q2,np.linspace(0,10,101))
+        print("rotation",rotation,"\n")
+        pbRotation = convertTo_pybulletQuat(rotation)
+        print("pbRotation",pbRotation,"\n")
