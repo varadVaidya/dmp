358796d0e270ddb07c0ebd68b2c15db5e2054106
Varad <vaidyavarad2001@gmail.com>
2021-07-17 16:10:50 +0530

    removed dmp1D

--- dmp_1D.py
+++
@@ -1,9 +0,0 @@
-## code to execut the dmp motion in 1D
-
-from dynamical_systems.forcingfuncs import Gaussian , get_weights_from_forcing_functions
-from dynamical_systems.canonical import FirstOrderDynamicalSystems, SecondOrderDynamicalSystem
-import numpy as np
-
-
-
-
