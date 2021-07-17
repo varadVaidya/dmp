da46f9174f9d31e1fb90c6afea9d6067973536d3
Varad <vaidyavarad2001@gmail.com>
2021-07-17 20:16:08 +0530

    added jacobian function

--- utils/manipulation/kinematics.py
+++ utils/manipulation/kinematics.py
@@ -2,19 +2,61 @@ import pybullet as pb
 import numpy as np
 
 class Kinematics():
-    def __init__(self):
+    def __init__(self):        
         pass
     
-    def endEffectorPos(self,armID,endEffectorIndex):
-        endEffector = pb.getLinkState(armID,endEffectorIndex,computeForwardKinematics=True)
+    def jointState(self):
+        joint = pb.getJointStates(self.armID,self.controlJoints)
+        
+        jointAngles = [ i[0] for i in joint]
+        jointVelocities = [ i[1] for i in joint]
+        jointReactionForces = [ i[2] for i in joint]
+        
+        self.jointAngles = jointAngles
+        self.jointVelocities = jointVelocities
+        self.jointReactionForces = jointReactionForces
+                
+        
+    def endEffectorPos(self):
+        
+        endEffector = pb.getLinkState(self.armID,self.endEffectorIndex,computeForwardKinematics=True)
         self.linkPosition = endEffector[4]
         self.linkOrientation = endEffector[5]
+        
     
-    def inverseKinematics(self,armID,endEffectorIndex,linkPosition,linkOrientation):
+    def getGeometricJacobian(self):
+        endEffector = pb.getLinkState(self.armID,self.endEffectorIndex)
+        
+        jointAngles = self.jointAngles
+        
+        linJac,angJac = pb.calculateJacobian(self.armID,self.endEffectorIndex,endEffector[2],jointAngles,self.controlZero,self.controlZero)
+
+        self.geometricJacobian = np.vstack((linJac,angJac))
+        self.geometricJacobianInv = np.linalg.pinv(self.geometricJacobian)
+        
+    
+    ## utils functions
+    def inverseKinematics(self,armID,endEffectorIndex,linkPosition,linkOrientation,ll,ul,jr,rp):
         if linkOrientation is not None:
             jointPosition = pb.calculateInverseKinematics(armID,endEffectorIndex,linkPosition,linkOrientation)
             self.inv_jointPosition = jointPosition
         else:
             jointPosition = pb.calculateInverseKinematics(armID,endEffectorIndex,linkPosition)
             self.inv_jointPosition = jointPosition
-            
+
+    def setParams(self,armID,endEffectorIndex,controlJoints,controlZero):
+        
+        self.armID = armID
+        self.endEffectorIndex = endEffectorIndex
+        self.controlJoints = controlJoints
+        self.controlZero = controlZero           
+        
+        self.jointState()
+        self.endEffectorPos()
+        self.getGeometricJacobian()
+        
+    
+    
+    
+        
+               
--- utils/manipulation/manipulator.py
+++ utils/manipulation/manipulator.py
@@ -7,12 +7,13 @@ from utils.manipulation.kinematics import Kinematics
 
 class Manipulator():
     
-    def __init__(self):
+    def __init__(self,initJointAngles = None,initEndeffectorPos = None,initEndeffectorOrientation = None):
         ## init kuka robot and plane.
         pb.connect(pb.GUI)
         pb.setAdditionalSearchPath(pybullet_data.getDataPath())
         self.armID = pb.loadURDF("kuka_iiwa/model.urdf",[0,0,0],useFixedBase=True)
         self.plane = pb.loadURDF("plane.urdf")
+        print(self.armID , "arm id")
         ## add gravity
         pb.setGravity(0,0,-9.81)
         pb.setRealTimeSimulation(False)
@@ -22,6 +23,9 @@ class Manipulator():
         self.totalJoints = pb.getNumJoints(self.armID)
         self.baseName = pb.getBodyInfo(self.armID)
         
+        # self.initJointAngles = initJointAngles
+        # self.initEndeffectorPos = initEndeffectorPos
+        # self.initEndEffectorOrientation = initEndeffectorOrientation
         # set the end effector link .
         self.endEffectorIndex = -1
         
@@ -38,22 +42,62 @@ class Manipulator():
                 self.endEffectorIndex = i
 
         self.controlJoints = np.array(self.controlJoints)
+        self.ndof = len(self.controlJoints)
+        self.controlZero = [0] * self.ndof
         
+        #lower limits for null space
+        self.ll = [-.967, -2, -2.96, 0.19, -2.96, -2.09, -3.05]
+        #upper limits for null space
+        self.ul = [.967, 2, 2.96, 2.29, 2.96, 2.09, 3.05]
+        #joint ranges for null space
+        self.jr = [5.8, 4, 5.8, 4, 5.8, 4, 6]
+        #restposes for null space
+        self.rp = [0, 0, 0, 0.5 * np.pi, 0, -np.pi * 0.5 * 0.66, 0]
         ## 
         self.kinematics = Kinematics()
+        
+        self.setInitPos(initJointAngles,initEndeffectorPos,initEndeffectorOrientation)
+        self.setParams()
     
-    def getForwardKinematics(self):
-        self.kinematics.endEffectorPos(self.armID,self.endEffectorIndex)
+    def setInitPos(self,jointAngles = None, endEffectorPos = None,endEffectorOrientation=None):
+        
+        if endEffectorPos is None and jointAngles is not None:
+            
+            pb.setJointMotorControlArray(self.armID,self.controlJoints,pb.POSITION_CONTROL,targetPositions =jointAngles)
+
+            for i in range(200):
+                pb.stepSimulation()
+            
+        if jointAngles is None and endEffectorPos is not None:
+            
+            self.kinematics.inverseKinematics(self.armID,self.endEffectorIndex,endEffectorPos,endEffectorOrientation,
+                                                          self.ll,self.ul,self.jr,self.rp)
+            
+            pb.setJointMotorControlArray(self.armID,self.controlJoints,pb.POSITION_CONTROL,targetPositions =self.kinematics.inv_jointPosition)
+            for i in range(200):
+                pb.stepSimulation()      
+           
+    def setParams(self):
+        
+        ## kinematics that are set here are:
+        ##      jointAngles
+        ##      end effector position
+        ##      geometric jacobian
+        self.kinematics.setParams(self.armID,self.endEffectorIndex,self.controlJoints,self.controlZero)
+        
+        
+    # def getForwardKinematics(self):
+    #     self.kinematics.endEffectorPos(self.armID,self.endEffectorIndex)
 
     def getInverseKinematics(self,linkPosition,linkOrientation = None):
-        self.kinematics.inverseKinematics(self.armID,self.endEffectorIndex,linkPosition,linkOrientation)
+        self.kinematics.inverseKinematics(self.armID,self.endEffectorIndex,linkPosition,linkOrientation,self.ll,self.ul,self.jr,self.rp)
     
 
 if __name__ == "__main__":
     
+    print("end effector position: FFFFFFF")
     kuka = Manipulator()
-    kuka.getForwardKinematics()
-    print(kuka.kinematics.linkPosition)      
+    print(kuka.kinematics.geometricJacobian)
         
         
         
